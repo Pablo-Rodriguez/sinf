f21fc79a45988bf8be4d42a347bed3412499302e
Pablo Sio <pablosio96@gmail.com>
2017-05-02 11:46:52 +0200

    Cambio de abierto a ccerrado e inversa añadido a triggers

--- src/db_triggers.sql
+++ src/db_triggers.sql
@@ -9,6 +9,10 @@ delimiter //
 create trigger trigger_reserva  after insert on reserva for each row
 begin
     declare s varchar(15);
+    declare c int default 0;
+    declare x int default 0;
+    select id_evento from grada where id_grada=NEW.id_grada into c;
+    select count(l.id_localidad) from localidad_grada l left join grada g on g.id_grada=l.id_grada where g.id_evento=c and estado ='libre' into x;
     select tipo from reserva where id_localidad=NEW.id_localidad into s;
     if s='prerreserva' then
         update localidad_grada
@@ -17,12 +21,20 @@ begin
         update localidad_grada
         set estado = 'reservado' where id_localidad=NEW.id_localidad;
     end if;
+    if x=1 then
+    update evento
+            set estado = 'cerrado' where id_evento = c;
+    end if;
 end //
 
 create trigger trigger_anulacion before delete on reserva for each row
 begin
-    update localidad_grada
+declare c int default 0;
+select id_evento from grada where id_grada=OLD.id_grada into c;
+update localidad_grada
     set estado = 'libre' where id_localidad = OLD.id_localidad;
-end //
+update evento
+        set estado = 'abierto' where id_evento = c;
+end//
 
 delimiter ;
